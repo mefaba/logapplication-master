26e05d98544b6642603ac13ab7f6e1b0ca3f3c6b
Marcus Bayraktar <mehmetfatih.bayraktar@ricoh.ca>
2024-02-06 16:53:36 -0500

    fix 5

--- src/test/java/com/example/logapp/LogControllerTest.java
+++ src/test/java/com/example/logapp/LogControllerTest.java
@@ -1,26 +1,59 @@
 package com.example.logapp;
 
+import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 
 import java.io.IOException;
+import java.time.LocalDate;
+import java.time.LocalDateTime;
+import java.time.format.DateTimeFormatter;
+
 class LogControllerTest {
-    LogController LogController = new LogController();
+    LogController LogController;
+
+    @BeforeEach
+    void setUp() {
+        LogController = new LogController();
+    }
     @Test
-    void processLogEntries() throws IOException {
-        // Define the date format
-//        DateTimeFormatter formatter = DateTimeFormatter.ofPattern("MMMM d, yyyy");
-//        // Parse the strings to LocalDate
-//        LocalDate date1 = LocalDate.parse("January 1, 2024", formatter);
-//        LocalDate date2 = LocalDate.parse("January 26, 2024", formatter);
-//        LogController.processLogEntries("C:\\Temp\\core_cuvwa00a3106.ent.wfb.bank.corp12152023_122739\\debug_core.log.20231212.1", "ServerCertRenewalTimer",date1,date2);
+    void processLogEntries_ValidInput_ValidOutput_Scenario1() throws IOException {
+        // Data for testing
+        DateTimeFormatter formatter = DateTimeFormatter.ofPattern("MMMM d yyyy H:m");
+        LocalDateTime date1 = LocalDateTime.parse("January 1 2024 00:00", formatter);
+        LocalDateTime date2 = LocalDateTime.parse("January 26 2024 00:00", formatter);
+        String[] includeTerms = "ServerCertRenewalTimer".split(", ");
+        String[] excludeTerms ="[DEBUG]".split(", ");
+        String filepath = "src/test/resources/debug_dm.log.20240119.8";
+        //Run unit test
+        LogController.processLogEntries(filepath, includeTerms, excludeTerms,date1,date2);
 
     }
 
     @Test
-    void isDateInRange() {
+    void processLogEntries_ValidInput_ValidOutput_Scenario2() throws IOException {
+        // Data for testing
+        DateTimeFormatter formatter = DateTimeFormatter.ofPattern("MMMM d yyyy H:m");
+        LocalDateTime date1 = LocalDateTime.parse("January 1 2024 00:00", formatter);
+        LocalDateTime date2 = LocalDateTime.parse("January 26 2024 00:00", formatter);
+        String[] includeTerms = "ServerCertRenewalTimer".split(", ");
+        String[] excludeTerms ="[DEBUG]".split(", ");
+        String filepath = "src/test/resources/debug_dm.log.20240119.8";
+        //Run unit test
+        LogController.processLogEntries(filepath, includeTerms, excludeTerms,date1,date2);
+
     }
 
     @Test
-    void writeResultToFile() {
+    void processLogEntries_ValidInput_ValidOutput_Scenario3() throws IOException {
+        // Data for testing
+        DateTimeFormatter formatter = DateTimeFormatter.ofPattern("MMMM d yyyy H:m");
+        LocalDateTime date1 = LocalDateTime.parse("January 1 2024 00:00", formatter);
+        LocalDateTime date2 = LocalDateTime.parse("January 26 2024 00:00", formatter);
+        String[] includeTerms = "ServerCertRenewalTimer".split(", ");
+        String[] excludeTerms ="[DEBUG]".split(", ");
+        String filepath = "src/test/resources/debug_dm.log.20240119.8";
+        //Run unit test
+        LogController.processLogEntries(filepath, includeTerms, excludeTerms,date1,date2);
+
     }
 }
